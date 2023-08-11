e6feea50e62d292225d4b94b1f36ed8389e69b99
k5jae <k5jae@stutzman.net>
2023-08-11 17:38:56 -0500

    [WIP] cleanup and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 // show logs when debugging
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use iced::{executor, Padding, theme, Color};
-use iced::{Application, Command, Length, Alignment, 
-    Element, Settings, Theme, Subscription};
+use iced::widget::{
+    button, column, horizontal_space, /* vertical_rule */
+    /* Text, */ progress_bar, /* slider, */
+    radio, row, text, Container,
+};
 use iced::window::icon::from_rgba;
 use iced::window::{Icon, Settings as Window};
-use iced::widget::{Container, /* Text, */ progress_bar, /* slider, */ 
-    radio, button, column, row, text, horizontal_space, /* vertical_rule */};
+use iced::{executor, theme, Color, Padding};
+use iced::{Alignment, Application, Command, Element, Length, Settings, Subscription, Theme};
 use iced_aw::{split, Split};
 use image::{self, GenericImageView};
-use tracing::debug;
 use rfd::AsyncFileDialog;
+use tracing::debug;
 
 fn icon() -> Icon {
     let image = image::load_from_memory(include_bytes!("../res/img/logo/icon.png")).unwrap();
@@ -30,7 +32,7 @@ pub fn main() -> iced::Result {
             resizable: true,
             decorations: true,
             icon: Some(icon()),
-            min_size: Some((300,300)),
+            min_size: Some((300, 300)),
             ..iced::window::Settings::default()
         },
         // try_opengles_first: true,
@@ -39,7 +41,7 @@ pub fn main() -> iced::Result {
         ..iced::Settings::default()
     };
 
-    return  App::run(settings);
+    return App::run(settings);
 }
 
 struct App {
@@ -64,7 +66,6 @@ pub enum Message {
     OpenFile(Option<String>),
 }
 
-
 impl Application for App {
     type Executor = executor::Default;
     type Flags = ();
@@ -84,13 +85,13 @@ impl Application for App {
 
     fn new(_flags: ()) -> (App, Command<Self::Message>) {
         (
-            Self { 
+            Self {
                 value: 0,
                 progress: 0.0,
                 ver_divider_position: Some(150),
                 selection: None,
                 flashing: false,
-            }, 
+            },
             Command::none(),
         )
     }
@@ -99,64 +100,60 @@ impl Application for App {
         String::from("OpenRTX Companion")
     }
 
-fn update(&mut self, message: Message) -> Command<Self::Message> {
-    match message {
-        Message::IncrementPressed => {
-            self.value += 1;
-            debug!("Inc");
-        },
-        Message::DecrementPressed => {
-            self.value -= 1;
-            debug!("Dec");
-        },
-        Message::OnVerResize(_position) => self.ver_divider_position = Some(150), //Some(position),
-        Message::SliderChanged(x) => self.progress = x,
-        Message::LanguageSelected(radio_hw) => {
-            self.selection = Some(radio_hw);
-        },
-        Message::Tick => {
-            if self.flashing {
-                self.progress += 5.0;
-                debug!("update progress...");
-                if self.progress > 100.0 {
-                    self.flashing = false;
+    fn update(&mut self, message: Message) -> Command<Self::Message> {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+                debug!("Inc");
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+                debug!("Dec");
+            }
+            Message::OnVerResize(_position) => self.ver_divider_position = Some(150), //Some(position),
+            Message::SliderChanged(x) => self.progress = x,
+            Message::LanguageSelected(radio_hw) => {
+                self.selection = Some(radio_hw);
+            }
+            Message::Tick => {
+                if self.flashing {
+                    self.progress += 5.0;
+                    debug!("update progress...");
+                    if self.progress > 100.0 {
+                        self.flashing = false;
+                    }
                 }
             }
-        },
-        Message::FlashPressed => {
-            self.progress = 0.0;
-            self.flashing = true;
-            debug!("flash");
-        },
-        Message::OpenFWPressed => {
-            return Command::perform(
-                async {
-                    let file = AsyncFileDialog::new()
-                    .pick_file()
-                    .await;
-                    if let Some(file) = file {
-                        Some(format!(
-                            "file:///{}",
-                            file.path().to_str().unwrap().to_string()
-                        ))
-                    } else {
-                        None
-                    }
-                }, 
-                move |f| Message::OpenFile(f)
-            );
-            
-        },     
-        Message::OpenFile(f) => {
-            debug!(f);
-        }   
+            Message::FlashPressed => {
+                self.progress = 0.0;
+                self.flashing = true;
+                debug!("flash");
+            }
+            Message::OpenFWPressed => {
+                return Command::perform(
+                    async {
+                        let file = AsyncFileDialog::new().pick_file().await;
+                        if let Some(file) = file {
+                            Some(format!(
+                                "file:///{}",
+                                file.path().to_str().unwrap().to_string()
+                            ))
+                        } else {
+                            None
+                        }
+                    },
+                    move |f| Message::OpenFile(f),
+                );
+            }
+            Message::OpenFile(f) => {
+                debug!(f);
+            }
+        }
+        Command::none()
     }
-    Command::none()
-}
 
-fn view(&self) -> Element<Message> {
-
-    let first = Container::new(
+    fn view(&self) -> Element<Message> {
+        let first = Container::new(
             //Text::new("Left")
             column(
                 RadioHW::all()
@@ -171,74 +168,56 @@ fn view(&self) -> Element<Message> {
                         )
                     })
                     .map(Element::from)
-                    .collect()
+                    .collect(),
             )
-            .spacing(10)
+            .spacing(10),
         )
         .width(Length::Fill)
         .height(Length::Fill)
         .center_x()
         .center_y();
 
-    let second = Container::new(
-        //     column![
-        //      button("Increment").on_press(Message::IncrementPressed),
-        //      text(self.value).size(50),
-        //      button("Decrement").on_press(Message::DecrementPressed),
-        //      progress_bar(0.0..=100.0, self.progress),
-        //      slider(0.0..=100.0, self.progress, Message::SliderChanged).step(0.01)
-        //  ])
-        column![
-            row![
-                //row![
-                    column![
-                        text("Version:").size(15),
-                    ]
-                    .padding(Padding::from([0, 10, 0, 0]))
-                    .width(80),
-                    column![
-                        text("0.3.0").size(15),
-                    ]
-                //]
-            ],
-            row![
-                //row![
-                    column![
-                        text("Device:").size(15),
-                    ]
-                    .padding(Padding::from([0, 10, 0, 0]))
-                    .width(80),
-                    column![
-                        text("/dev/ttyACM0, Fast, not slow, 16MB flash, etc ").size(15),
-                    ]
-                //]
-            ],
-            row![
-                //row![
-                    column![
-                        text("Notes:").size(15),
-                    ]
-                    .padding(Padding::from([0, 10, 0, 0]))
-                    .width(80),
-                    column![
-                        text("What it is").size(15),
-                    ]
-                //]
-            ],
+        let second = Container::new(
+            //     column![
+            //      button("Increment").on_press(Message::IncrementPressed),
+            //      text(self.value).size(50),
+            //      button("Decrement").on_press(Message::DecrementPressed),
+            //      progress_bar(0.0..=100.0, self.progress),
+            //      slider(0.0..=100.0, self.progress, Message::SliderChanged).step(0.01)
+            //  ])
             column![
-                progress_bar(0.0..=100.0, self.progress),
                 row![
-                    button("Open FW File").on_press(Message::OpenFWPressed),
-                    horizontal_space(10),
-                    button("Flash Radio").on_press(Message::FlashPressed),
-                ].padding(15)
-                
+                    column![text("Version:").size(15),]
+                        .padding(Padding::from([0, 10, 0, 0]))
+                        .width(80),
+                    column![text("0.3.0").size(15),]
+                ],
+                row![
+                    column![text("Device:").size(15),]
+                        .padding(Padding::from([0, 10, 0, 0]))
+                        .width(80),
+                    column![text("/dev/ttyACM0, Fast, not slow, 16MB flash, etc ").size(15),]
+                ],
+                row![
+                    column![text("Notes:").size(15),]
+                        .padding(Padding::from([0, 10, 0, 0]))
+                        .width(80),
+                    column![text("What it is").size(15),]
+                ],
+                column![
+                    progress_bar(0.0..=100.0, self.progress),
+                    row![
+                        button("Open FW File").on_press(Message::OpenFWPressed),
+                        horizontal_space(10),
+                        button("Flash Radio").on_press(Message::FlashPressed),
+                    ]
+                    .padding(15)
+                ]
+                .padding(Padding::from([40, 0, 0, 0]))
+                .align_items(Alignment::Center)
             ]
-            .padding(Padding::from([40, 0, 0, 0]))
-            .align_items(Alignment::Center)
-        ]
-        .padding(20)
-        .align_items(Alignment::Start)
+            .padding(20)
+            .align_items(Alignment::Start),
         )
         .width(Length::Fill)
         .height(Length::Fill)
@@ -252,33 +231,29 @@ fn view(&self) -> Element<Message> {
             split::Axis::Vertical,
             Message::OnVerResize,
         )
-        .into()        
+        .into()
 
-    // row![
-    //     column![
-    //         button("Increment").on_press(Message::IncrementPressed),
-    //         text(self.value).size(50),
-    //         button("Decrement").on_press(Message::DecrementPressed)
-    //     ],
-    //     vertical_rule(1),
-    //     column![
-    //         button("Increment").on_press(Message::IncrementPressed),
-    //         text("This is a test").size(15),
-    //         button("Decrement").on_press(Message::DecrementPressed)
-    //     ]
-    // ]
-    // .padding(20)
-    // .align_items(Alignment::Center)
-    // .into()
+        // row![
+        //     column![
+        //         button("Increment").on_press(Message::IncrementPressed),
+        //         text(self.value).size(50),
+        //         button("Decrement").on_press(Message::DecrementPressed)
+        //     ],
+        //     vertical_rule(1),
+        //     column![
+        //         button("Increment").on_press(Message::IncrementPressed),
+        //         text("This is a test").size(15),
+        //         button("Decrement").on_press(Message::DecrementPressed)
+        //     ]
+        // ]
+        // .padding(20)
+        // .align_items(Alignment::Center)
+        // .into()
+    }
 
-}
-
-fn subscription(&self) -> Subscription<Message> {
-    iced::time::every(std::time::Duration::from_millis(500)).map(|_| {
-        Message::Tick
-    })
-}
-
+    fn subscription(&self) -> Subscription<Message> {
+        iced::time::every(std::time::Duration::from_millis(500)).map(|_| Message::Tick)
+    }
 }
 
 fn init_logging() {
@@ -295,7 +270,6 @@ fn init_logging() {
     )
     .expect("setting default subscriber failed");
 }
-
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum RadioHW {
