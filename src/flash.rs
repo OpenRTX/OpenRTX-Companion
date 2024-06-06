d491635acf8712138d914f1c2c4954522c53f9d0
Niccolò Izzo <izzo.niccolo@gmail.com>
2024-06-06 17:47:33 +0200

    Fix tab compilation errors
    Now the thing compiles, need to work quite some to re-establish previous
    functionality.

--- src/flash.rs
+++ src/flash.rs
@@ -1,24 +1,19 @@
 // show logs when debugging
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use iced::widget::{
-    button, column, horizontal_space, /* vertical_rule */
-    /* Text, */ progress_bar, /* slider, */
-    radio, row, text, Container,
-    combo_box,
+use iced::{
+    alignment::{Horizontal, Vertical},
+    Alignment, Application, Command, Element, Length,
+    widget::{Container, Button, Row, Column, column, Text, TextInput, radio}
 };
-use iced::window::icon::from_rgba;
-use iced::window::{Icon, Settings as Window};
-use iced::{executor, theme, Color, Padding};
-use iced::{Alignment, Application, Command, Element, Length, Settings, Subscription, Theme};
-use iced_aw::{split, Split, TabLabel, Tabs};
+use iced_aw::TabLabel;
 use image::{self, GenericImageView};
 use rfd::AsyncFileDialog;
 use tracing::debug;
-use rtxflash::{self, get_devices};
-// use rtxlink::{flow, link};
-use serial_enumerator::get_serial_list;
-use std::sync::mpsc::{channel, Receiver};
+use rtxflash::{self, get_devices, get_device_info, flash_device};
+use rtxflash::radio_tool_ffi::DeviceInfo;
+
+use crate::{Icon, Message, Tab};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum RadioHW {
@@ -47,38 +42,43 @@ impl From<RadioHW> for String {
     }
 }
 
+#[derive(Clone, Debug)]
+pub enum FlashMessage {
+    ModelChanged(RadioHW),
+    DeviceChanged(u64),
+    OpenFWPressed,
+    OpenFile(Option<String>),
+    FlashPressed,
+    Tick,
+}
+
 pub struct FlashTab {
-    devices: DeviceInfo,
-    progress: f32,
+    devices: Vec<DeviceInfo>,
+    selected_model: Option<RadioHW>,
+    selected_device: Option<u16>,
+    firmware_path: Option<String>,
     flash_in_progress: bool,
+    progress: f32,
 }
 
 impl FlashTab {
     pub fn new() -> Self {
         FlashTab {
+            devices: get_devices(),
+            selected_model: None,
+            selected_device: None,
+            firmware_path: None,
             progress: 0.0,
             flash_in_progress: false,
         }
     }
 
-    pub fn update(&mut self, message: LoginMessage) {
+    pub fn update(&mut self, message: FlashMessage) {
         match message {
-            FlashMessage::FlashPressed => {
-                self.progress = 0.0;
-                self.flash_in_progress = true;
-                self.backup_in_progress = false;
-                debug!("flash");
-                // TODO: flash_in_progress
-                println!("Flashing OpenRTX firmware");
-                if let Err(err) = ffi::flash_radio() {
-                    eprintln!("Error: {}", err);
-                    // process::exit(1);
-                }
-                println!("Firmware flash completed");
-                println!("Please reboot the radio");
-            }
+            FlashMessage::ModelChanged(model) => { }
+            FlashMessage::DeviceChanged(model) => { }
             FlashMessage::OpenFWPressed => {
-                return Command::perform(
+                Command::perform(
                     async {
                         let file = AsyncFileDialog::new().pick_file().await;
                         if let Some(file) = file {
@@ -90,11 +90,32 @@ impl FlashTab {
                             None
                         }
                     },
-                    move |f| Message::OpenFile(f),
+                    move |f| FlashMessage::OpenFile(f),
                 );
+                ()
+            }
+            FlashMessage::FlashPressed => {
+                self.progress = 0.0;
+                self.flash_in_progress = true;
+                debug!("flash");
+                // TODO: flash_in_progress
+                println!("Flashing OpenRTX firmware");
+                flash_device(self.selected_device.unwrap(), self.firmware_path.as_mut().unwrap().as_ref());
+                println!("Firmware flash completed");
+                println!("Please reboot the radio");
             }
             FlashMessage::OpenFile(file) => {
                 debug!(file);
+                self.firmware_path = file;
+            }
+            FlashMessage::Tick => {
+                if self.flash_in_progress {
+                    self.progress += 5.0;
+                    debug!("update progress...");
+                    if self.progress > 100.0 {
+                        self.flash_in_progress = false;
+                    }
+                }
             }
         }
     }
@@ -113,41 +134,42 @@ impl Tab for FlashTab {
     }
 
     fn content(&self) -> Element<'_, Self::Message> {
-        let content: Element<'_, LoginMessage> = Container::new(
+        let content: Element<'_, FlashMessage> = Container::new(
             Column::new()
                 .align_items(Alignment::Center)
                 .max_width(600)
                 .padding(20)
                 .spacing(16)
-                .push(
-                    TextInput::new("Username", &self.username)
-                        .on_input(LoginMessage::UsernameChanged)
-                        .padding(10)
-                        .size(32),
-                )
-                .push(
-                    TextInput::new("Password", &self.password)
-                        .on_input(LoginMessage::PasswordChanged)
-                        .padding(10)
-                        .size(32)
-                        .secure(true),
-                )
+                // TODO: Replace these with radio buttons
+                //.push(
+                //    TextInput::new("Model", &self.selected_model)
+                //        .on_input(FlashMessage::ModelChanged)
+                //        .padding(10)
+                //        .size(32),
+                //)
+                //.push(
+                //    TextInput::new("Device", &self.selected_device)
+                //        .on_input(FlashMessage::DeviceChanged)
+                //        .padding(10)
+                //        .size(32)
+                //        .secure(true),
+                //)
                 .push(
                     Row::new()
                         .spacing(10)
                         .push(
                             Button::new(
-                                Text::new("Clear").horizontal_alignment(Horizontal::Center),
+                                Text::new("Select Firmware").horizontal_alignment(Horizontal::Center),
                             )
                             .width(Length::Fill)
-                            .on_press(LoginMessage::ClearPressed),
+                            .on_press(FlashMessage::OpenFWPressed),
                         )
                         .push(
                             Button::new(
-                                Text::new("Login").horizontal_alignment(Horizontal::Center),
+                                Text::new("Flash").horizontal_alignment(Horizontal::Center),
                             )
                             .width(Length::Fill)
-                            .on_press(LoginMessage::LoginPressed),
+                            .on_press(FlashMessage::FlashPressed),
                         ),
                 ),
         )
@@ -155,6 +177,6 @@ impl Tab for FlashTab {
         .align_y(Vertical::Center)
         .into();
 
-        content.map(Message::Login)
+        content.map(Message::Flash)
     }
 }
